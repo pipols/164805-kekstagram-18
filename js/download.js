45dddae1f505da4495372da47195c0fc165cb02b
pipols <pipols2007@gmail.com>
2019-11-03 20:11:43 +0300

    правки по :Техническое задание 5.4

--- js/download.js
+++ js/download.js
@@ -6,12 +6,12 @@
   };
   var start = function () {
     window.backend.download(getData, window.alert.getErrorMessage);
-    window.filter.showFilters();
   };
 
   var getData = function (data) {
     window.gallery.renderGallery(data);
     window.download.data = data;
+    window.filter.showFilters();
   };
 
   start();
